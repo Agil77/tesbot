c54555b10dd9f05d74309b7f6021543eeafb3585
Agil77 <108315883+Agil77@users.noreply.github.com>
2022-11-24 19:34:51 +0700

    Create own-addprem.js

---
+++ plugins/own-addprem.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = db.data.users[who]
+    if (!who) throw `tag atau balas pesan yang mau dijadikan premium!`
+     let txt = text.replace('@' + who.split`@`[1], '').trim()
+     
+    
+    if (isNaN(txt)) return m.reply(`hanya nomor mamaskuh!\n\ncontoh:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
+     var jumlahHari = 86400000
+    var now = new Date() * 1
+    if (user.premiumTime > 0) throw `You have already claimed this daily claim!, wait for *${((user.premiumTime + 0) - new Date()).toTimeString()}*`
+    if (now < user.premiumTime) user.premiumTime += jumlahHari
+    else user.premiumTime = now + jumlahHari
+    user.premium = true
+    m.reply(`Berhasil!\n*${user.name}* sekarang sudah premium  1 hari.`)
+    
+}
+handler.help = ['addprem [@user] <hari>']
+handler.tags = ['owner']
+handler.command = /(PjwMnBFTfwS)/i
+handler.private = true
+
+
+export default handler
